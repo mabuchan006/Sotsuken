11b71f75b2b3d9b46ea07299bda7dffa2e51b642
1211020 <1211020@A40242.student.ncs.ac.jp>
2015-11-16 12:26:07 +0900

    カンマ区切り修正

--- WebContent/js/drag_drop/NCS8/NCS802_1.js
+++ WebContent/js/drag_drop/NCS8/NCS802_1.js
@@ -1,27 +1,19 @@
-var commaFlagNCS802_1 = new boolean();
-
-function NCS802_1_hold( e ){
-	if($("#hold").val()=="true"){
-		commaFlagNCS802_1=true;
-	}
-}
+var repComma;
+var str;
 
 //ドロップ
 function NCS802_1_drop( e ) {
 	$( "#NCS802_1-drop" ).droppable({
-		activeClass: "ui-state-default",
-		hoverClass: "ui-state-hover",
-		accept: ":not(.ui-sortable-helper)",
 		drop: function( e, ui ) {
 
-			//カンマ区切り判定
-			if( commaFlagNCS802_1 ) {
-				$( "#NCS802_1"  ).text( $( "#NCS802_1"  ).text() + "," + ui.draggable.text() ).appendTo( this );
-			} else {
-				$( "#NCS802_1" ).text( ui.draggable.text() ).appendTo( this );
-				commaFlagNCS802_1 = true;
-				$("#holdNCS802_1").val( commaFlagNCS802_1 );
+			str = ($( "#NCS802_1" ).text() + ui.draggable.text() );
+
+			repComma = str.replace(/,/g, "");
+			while( repComma != repComma.replace( /(\w+)(\w{4})/, "$1,$2" ) ) {
+				repComma = repComma.replace( /(\w+)(\w{4})/, "$1,$2" )
 			}
+
+			$( "#NCS802_1" ).text( repComma ).appendTo( this );
 
 		}
 	})
