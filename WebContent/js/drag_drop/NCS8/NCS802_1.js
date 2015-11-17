d967d2a64f249147732d52673dba6489704cfd3c
tetsuya <tetsuya@192.168.1.2>
2015-11-17 10:03:25 +0900

    コメント付加、座標から要素取得（できない）

--- WebContent/js/drag_drop/NCS8/NCS802_1.js
+++ WebContent/js/drag_drop/NCS8/NCS802_1.js
@@ -6,13 +6,17 @@ function NCS802_1_drop( e ) {
 	$( "#NCS802_1-drop" ).droppable({
 		drop: function( e, ui ) {
 
+			//テキストボックスの中身とドロップされたテキストを入れる
 			str = ($( "#NCS802_1" ).text() + ui.draggable.text() );
 
+			//一度カンマを取り除く
 			repComma = str.replace(/,/g, "");
+			//下４桁ずつカンマ区切りにしていく
 			while( repComma != repComma.replace( /(\w+)(\w{4})/, "$1,$2" ) ) {
 				repComma = repComma.replace( /(\w+)(\w{4})/, "$1,$2" )
 			}
 
+			//テキストボックスに追加
 			$( "#NCS802_1" ).text( repComma ).appendTo( this );
 
 		}
