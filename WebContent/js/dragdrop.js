11d826718a9850eb28af516844852890308cd6d0
1211020 <1211020@A40242.student.ncs.ac.jp>
2015-11-20 13:50:02 +0900

    同値チェック実装

--- WebContent/js/dragdrop.js
+++ WebContent/js/dragdrop.js
@@ -14,25 +14,23 @@ function f_drag( e ) {
 function f_drop( e ) {
 	$( ".drop-target" ).droppable({
 		drop: function( e, ui ) {
-
-			//子要素の取得
-			ele = $(this).children("textarea").get(0);
-			console.log(ele);
-			//テキストボックスの中身とドロップされたテキストを入れる
-			str = ( ele.value + ui.draggable.text() );
-
-			//一度カンマを取り除く
-			repComma = str.replace(/,/g, "");
-
-			//下４桁ずつカンマ区切りにしていく
-			while( repComma != repComma.replace( /(\w+)(\w{4})/, "$1,$2" ) ) {
-				repComma = repComma.replace( /(\w+)(\w{4})/, "$1,$2" )
-			}//while
-
-			//テキストボックスに追加
-			ele.value = repComma;
-			console.log(ele.value);
-
+			//同値チェック
+			if( repComma.indexOf( ui.draggable.text() ) === -1 ){
+				//子要素の取得
+				ele = $(this).children("textarea").get(0);
+				//テキストボックスの中身とドロップされたテキストを入れる
+				str = ( ele.value + ui.draggable.text() );
+				//一度カンマを取り除く
+				repComma = str.replace(/,/g, "");
+				//下４桁ずつカンマ区切りにしていく
+				while( repComma != repComma.replace( /(\w+)(\w{4})/, "$1,$2" ) ) {
+					repComma = repComma.replace( /(\w+)(\w{4})/, "$1,$2" )
+				}//while
+				//テキストボックスに追加
+				ele.value = repComma;
+			} else {
+				disabled: true;
+			}//if
 		}//drop
 	})//droppable
 }//f_drop
