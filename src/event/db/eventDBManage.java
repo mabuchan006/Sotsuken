f3c7fe95783afdd8fcebeda46741118888980b29
1211085 <1211085@A40241.student.ncs.ac.jp>
2015-11-25 12:23:50 +0900

    event

--- src/event/db/eventDBManage.java
+++ src/event/db/eventDBManage.java
@@ -4,10 +4,7 @@ import DB.DBAccess;
 
 public class  eventDBManage extends DBAccess{
 	private final static String DRIVER_NAME = "java:comp/env/jdbc/MySqlCon";//コネクタ
-	public eventDBManage( ) {
 
-		// TODO 自動生成されたコンストラクター・スタブ
-	}
 	private String selectSql;//クラス全検索用
 	private String deleteSql;//クラス1件削除用
 	private String insertSql;//クラス1件登録用
