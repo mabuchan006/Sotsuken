af070a05a822d6656c81556b80a8739c716324e5
1211093 <1211093@A40234.student.ncs.ac.jp>
2015-11-26 14:40:58 +0900

    定期コミット

--- WebContent/js/belongings.js
+++ WebContent/js/belongings.js
@@ -1,18 +1,21 @@
-//timeTableのinfo1のtdへ連番idを付与するメソッド
-//tdの数だけ連番を与える
-$('info1').children().each(function (i) {
-	$(this).attr( 'id' , 'cell' + (i+1) );
+////timeTableのinfo1のtdへ連番idを付与するメソッド
+////tdの数だけ連番を与える
+//$('info1').children().each(function (i) {
+//	$(this).attr( 'id' , 'cell' + (i+1) );
+//});
+//
+////tdをクリックしたら、その要素の情報がele内に格納され、ポップアップが表示される
+//$('#timeTable info1 td').on('click' , function() {
+//
+//	//ele = <td id="cell1">授業</td>
+//	var ele = $(this.id);
+//
+//	$(ele).after( ' data-content="教科書" ' );
+//	//ele = <td id="cell1"data-toggle="popover" title="持ち物" data-content="教科書">授業</td>
+//
+//	$(ele).popover();
+//
+//} );
+$( '#cell1 ').on( 'click' , function() {
+	$('#cell1').popover();
 });
-
-//tdをクリックしたら、その要素の情報がele内に格納され、ポップアップが表示される
-$('#timeTable info1 td').on('click' , function() {
-
-	//ele = <td id="cell1">授業</td>
-	var ele = $(this.id);
-
-	$(ele).after( 'data-toggle="popover" title="持ち物" data-content="教科書"' );
-	//ele = <td id="cell1"data-toggle="popover" title="持ち物" data-content="教科書">授業</td>
-
-	$(ele).popover();
-
-} );
