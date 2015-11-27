27d1539cdfa3d3e358d5130fbc1c9aa132ada2c6
1211093 <1211093@A40234.student.ncs.ac.jp>
2015-11-27 11:45:59 +0900

    kk

--- WebContent/js/belongings.js
+++ WebContent/js/belongings.js
@@ -1,19 +1,5 @@
-//tdの数だけ連番を与える
-//$('info ').children().each(function (i) {
-//	$(this).attr( 'id' , 'cell' + (i+1) );
-//});
-//
-////tdをクリックしたら、その要素の情報がele内に格納され、ポップアップが表示される
-//$('info  td').on('click' , function() {
-//
-//	//ele = <td id="cell1">授業</td>
-//	var ele = $(this.id);
-//
-//	$(ele).popover();
-//
-//} );
 
 $(function() {
 	//シングルクォーテーションの中に対応するid名を指定 ex→('#cell1')
-	$('#').popover();
+	$('#cell1').popover();
 })
