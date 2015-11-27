8122c1fdd646f80de93b6ee23a1471d9404403cc
1211093 <1211093@A40234.student.ncs.ac.jp>
2015-11-27 14:29:43 +0900

    js

--- WebContent/js/jPrintArea.js
+++ WebContent/js/jPrintArea.js
@@ -1,8 +1,7 @@
-jQuery.jPrintArea=function(el){
-	iframe.src="javascript:false;";
+$.jPrintArea=function(el){
 	var iframe=document.createElement('IFRAME');
 	var doc=null;
-	jQuery(iframe).attr('style','position:absolute;width:0px;height:0px;left:-500px;top:-500px;');
+	$(iframe).attr('style','position:absolute;width:0px;height:0px;left:-500px;top:-500px;');
 	document.body.appendChild(iframe);
 	doc=iframe.contentWindow.document;
 	var links=window.document.getElementsByTagName('link');
@@ -13,6 +12,5 @@ jQuery.jPrintArea=function(el){
 	doc.close();
 	iframe.contentWindow.focus();
 	iframe.contentWindow.print();
-	//alert('印刷しています...');
 	document.body.removeChild(iframe);
 }
