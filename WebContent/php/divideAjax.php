d8fd2b1558d0888f26cb56e78c4c2a1eb098c95d
1211035 <1211035@A40243.student.ncs.ac.jp>
2015-12-09 12:24:22 +0900

    php

--- WebContent/php/divideAjax.php
+++ WebContent/php/divideAjax.php
@@ -1,12 +1,28 @@
 <?php
-<<<<<<< HEAD
-phpinfo();
-=======
 
 mysql_set_charset('utf8');
-
+//データベース接続
 $connect=mysql_connect("localhost","kento","kkkk") or die("失敗");
 print "成功";
 
->>>>>>> branch 'master' of https://github.com/mabuchan006/Sotsuken.git
+
+//データベース選択
+$db_selected = mysql_select_db('database_name', $link);
+if (!$db_selected){
+	die('データベース選択失敗です。'.mysql_error());
+}
+
+//Ajaxから曜日情報受け取り
+$week=$_POST['week'];
+
+//曜日ごとのSELECT
+	$result = mysql_query('select r.roomName, d.period, c.classID from tbl_timedivide d, tbl_room r, tbl_class c
+											where d.roomID = r.roomID and d.classID = c.classID and week = $week');
+	if (!$result) {
+		die('クエリーが失敗しました。'.mysql_error());
+	}
+	mysql_close($link);
+
+
+
 ?>
