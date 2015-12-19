7f2ad2897c42aac144d751000ca5e3db0777e99e
tetsuya <tetsuya@192.168.1.2>
2015-12-19 15:42:47 +0900

    insert問題解決策模索中

--- WebContent/js/formCheck.js
+++ WebContent/js/formCheck.js
@@ -1,22 +1,43 @@
-var classIDArray = "", regExp = "";
+var regex = new RegExp(/([ajrs][1234][agm][12345],?)*/ig), txtVal = "", valArray = new Array, keyChar = "";
 function formCheck( e ) {
 
-	$("textarea").focusout(function(e){
-		regExp = /([agjmrs][1234][agjmrs][12345],?)*/ig;
-		console.log($(this).val());
-		if( !( regExp.test( $(this).val() ) ) ){
+	$("textarea").on({
+		//半角英数とエンターキーの入力を検知
+		"keypress" : function(e){
+			if( e != null){
+				keyChar = String.fromCharCode(e.which).toUpperCase();
+				if(keyChar == "A" || keyChar == "G" || keyChar == "J" || keyChar == "M" || keyChar == "R" || keyChar == "S"
+					|| keyChar == "1" || keyChar == "2" || keyChar == "3" || keyChar == "4" || keyChar == "5"){
+					txtVal += keyChar;
+					console.log(txtVal);
+					$("#checkFlag").get(0).value = "true";
+				}
+			}
+		},
+		//backspace key と delete key の入力を検知
+		"keyup" : function(e){
+			if(e.which === 8 || e.which === 46){
+				txtVal = $(this).val();
+				console.log(txtVal);
+			}
+			$("#checkFlag").get(0).value = "true";
+		},
+		"focusout" : function(e){
+			valArray = txtVal.match(regex);
+			txtVal = "";
+			var thisVal = "";
+			$.each(valArray, function(k,v){
+				$.each($(".drag-target li"), function(liKey, classID){
+					if(v.test(classID)){
+						thisVal += v;
+					}
+				})
+			})
+			thisVal = thisVal.replace(/,/g,"");
+			while(thisVal != thisVal.replace(/(\w+)(\w{4})/, "$1,$2")){
+				thisVal = thisVal.replace(/(\w+)(\w{4})/, "$1,$2");
+			}
+			$(this).val(thisVal);
 		}
 	})
-
-	//半角英数とエンターキーの入力を検知
-	$("textarea").keypress(function( e ){
-		$("#checkFlag").get(0).value = "true";
-	});
-
-	//backspace key と delete key の入力を検知
-	$("textarea").keyup(function( e ){
-		if(e.which === 8 || e.which === 46){
-			$("#checkFlag").get(0).value = "true";
-		}
-	});
 }
