09efb1fd80f45cbf8c9f0afced920f41b8740801
tetsuya <tetsuya@192.168.1.2>
2015-12-20 01:45:09 +0900

    ちょっと修正

--- WebContent/js/formCheck.js
+++ WebContent/js/formCheck.js
@@ -3,22 +3,23 @@ function formCheck( e ) {
 
 	$("textarea").on({
 		"focusin" : function(e) {
-			txtVal = $(this).val();
-		},
+			txtVal = $(this).val().toUpperCase();
+		},/**
 		//半角英数とエンターキーの入力を検知
 		"keypress" : function(e){
 			if( e != null){
 				keyChar = String.fromCharCode(e.which).toUpperCase();
 				if(keyChar == "A" || keyChar == "G" || keyChar == "J" || keyChar == "M" || keyChar == "R" || keyChar == "S"
 					|| keyChar == "1" || keyChar == "2" || keyChar == "3" || keyChar == "4" || keyChar == "5"){
-					txtVal += keyChar;
 					console.log(txtVal);
 					$("#checkFlag").get(0).value = "true";
 				}
 			}
-		},
+		},**/
 		//backspace key と delete key の入力を検知
 		"keyup" : function(e){
+			txtVal = $(this).val().toUpperCase();
+			console.log(txtVal);
 			if(e.which === 8 || e.which === 46){
 				txtVal = $(this).val();
 				console.log(txtVal);
