2c86489e3282d039553db74affccd5c1c24eac37
Shohei_P <Shohei_P@10.13.68.50>
2016-01-07 16:24:15 +0900

    ログアウト時の「ログアウトしました」メッセージ表示処理、ちょっとよくわからず

--- src/login/loginControl.java
+++ src/login/loginControl.java
@@ -29,7 +29,7 @@ public class loginControl extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		String loginPath = "/Sotsuken/manage/";//変更予定のため未記述
-		String logoutPath = "/Sotsuken/";//変更予定のため未記述
+		String logoutPath = "/Sotsuken/top.jsp";//変更予定のため未記述
 
 		//ログアウト処理
 		if( request.getParameter("state") != null &&
@@ -57,7 +57,7 @@ public class loginControl extends HttpServlet {
 	//クライアントから送られてきた情報を取得するような処理
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		System.out.println("１");
+		//System.out.println("１");
 		//パス初期値
 		String path="test/login_test.jsp";//変更予定のため未記述
 		String errPath="/Sotsuken/";//変更予定のため未記述
