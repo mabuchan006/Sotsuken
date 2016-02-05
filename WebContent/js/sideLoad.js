b3a653fab2e21f82ffbee1042e318a77923d4271
1211085 <1211085@A40241.student.ncs.ac.jp>
2016-02-05 11:18:42 +0900

    sidberコメントアウト

--- WebContent/js/sideLoad.js
+++ WebContent/js/sideLoad.js
@@ -1,39 +1,39 @@
-$(function(e){
-	$(".manage").click(function(e){
-		e.preventDefault();
-		var ele = $(this).children().children("a[href]");
-		window.setTimeout(pageLoad(ele.attr("href")), 2000);
-	});
-	$(".editView").click(function(e){
-		e.preventDefault();
-		var ele = $(this).children("a[href]");
-		window.setTimeout(pageLoad(ele.attr("href")), 2000);
-	});
-	$("h1 > .manage-top").click(function(e){
-		e.preventDefault();
-		var ele = $(this);
-		window.setTimeout(pageLoad(ele.attr("href")), 2000);
-	});
-})
-
-function pageLoad(url){
-	$.ajax({
-		type : "GET",
-		url : url,
-		dataType : "html",
-		timeout : 10000,
-		async : false
-	}).done(function(res){
-		$(".pure-pusher").html(res);
-		history.pushState(null, null, url);
-		location.reload();
-	}).fail(function(jqXHR, textStatus, errorThrown ){//失敗
-		console.log("NG:" + jqXHR.status + ":" + textStatus.status + ":" + errorThrown);
-		alert("エラーが発生しました\nサーバ管理者へ問い合わせてください");
-	});
-
-//	$(".pure-pusher").load(url,function() {
+//$(function(e){
+//	$(".manage").click(function(e){
+//		e.preventDefault();
+//		var ele = $(this).children().children("a[href]");
+//		window.setTimeout(pageLoad(ele.attr("href")), 2000);
+//	});
+//	$(".editView").click(function(e){
+//		e.preventDefault();
+//		var ele = $(this).children("a[href]");
+//		window.setTimeout(pageLoad(ele.attr("href")), 2000);
+//	});
+//	$("h1 > .manage-top").click(function(e){
+//		e.preventDefault();
+//		var ele = $(this);
+//		window.setTimeout(pageLoad(ele.attr("href")), 2000);
+//	});
+//})
+//
+//function pageLoad(url){
+//	$.ajax({
+//		type : "GET",
+//		url : url,
+//		dataType : "html",
+//		timeout : 10000,
+//		async : false
+//	}).done(function(res){
+//		$(".pure-pusher").html(res);
 //		history.pushState(null, null, url);
 //		location.reload();
+//	}).fail(function(jqXHR, textStatus, errorThrown ){//失敗
+//		console.log("NG:" + jqXHR.status + ":" + textStatus.status + ":" + errorThrown);
+//		alert("エラーが発生しました\nサーバ管理者へ問い合わせてください");
 //	});
-}
+//
+////	$(".pure-pusher").load(url,function() {
+////		history.pushState(null, null, url);
+////		location.reload();
+////	});
+//}
